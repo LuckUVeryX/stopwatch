79f820ac63902d2caad4c2288be5d3ad6cf19c71
RyanYipOsim <ryan.yip@osim.com.sg>
2021-12-08 17:39:30 +0800

    adds stopwatch consts

--- lib/app/stopwatch/widgets/stopwatch_secs_tick_marker.dart
+++ lib/app/stopwatch/widgets/stopwatch_secs_tick_marker.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import '../../../core/constants/stopwatch_constants.dart';
 import '../../../core/palette.dart';
 
 class StopwatchSecsTickMarker extends StatelessWidget {
@@ -12,24 +13,26 @@ class StopwatchSecsTickMarker extends StatelessWidget {
   final int milliseconds;
   final double radius;
 
-  static const _kWidth = 2.0;
-  static const _kSecondsHeight = 12.0;
-  static const _kMillisecondsHeight = 8.0;
-
   @override
   Widget build(BuildContext context) {
     // color ticker white every 5 seconds
     final color = milliseconds % 5000 == 0 ? Palette.kWhite : Palette.kGrey;
     // taller marker to indicate each second
-    final height =
-        milliseconds % 1000 == 0 ? _kSecondsHeight : _kMillisecondsHeight;
+    final height = milliseconds % 1000 == 0
+        ? StopwatchConst.kSecondsHeight
+        : StopwatchConst.kMillisecondsHeight;
+
     return Transform(
       alignment: Alignment.center,
       transform: Matrix4.identity()
-        ..translate(-_kWidth / 2, -height / 2, 0.0)
+        ..translate(-StopwatchConst.kTickerWidth / 2, -height / 2, 0.0)
         ..rotateZ(2 * pi * milliseconds / (60.0 * 1000.0))
         ..translate(0.0, radius - height / 2, 0.0),
-      child: Container(width: 2.0, height: height, color: color),
+      child: Container(
+        width: StopwatchConst.kTickerWidth,
+        height: height,
+        color: color,
+      ),
     );
   }
 }
---
+++ lib/core/constants/stopwatch_constants.dart
@@ -0,0 +1,9 @@
+class StopwatchConst {
+  static const kTickerWidth = 2.0;
+
+  static const kSecondsHeight = 12.0;
+  static const kMillisecondsHeight = 8.0;
+
+  static const kMinutesHeight = 4.0;
+  static const kHalfMinutesHeight = 2.0;
+}

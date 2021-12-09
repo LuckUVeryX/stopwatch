a3680449cec46c4abb0b78d63596a4aca897f364
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-12-09 12:04:16 +0800

    adds button ui

--- lib/app/stopwatch/views/stopwatch_page.dart
+++ lib/app/stopwatch/views/stopwatch_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../core/constants/stopwatch_constants.dart';
+import '../../../core/palette.dart';
+import '../../../core/widgets/timer_button.dart';
 import '../view_model/stopwatch_viewmodel.dart';
 import 'views.dart';
 
@@ -18,13 +21,39 @@ class StopwatchPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: AspectRatio(
-                  aspectRatio: 1 / 1,
+                  aspectRatio: 0.90,
                   child: LayoutBuilder(builder: (context, constraints) {
                     final radius = constraints.maxWidth / 2;
                     return Stack(
                       children: [
                         StopwatchRender(radius: radius),
                         StopwatchTickerUI(radius: radius),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: SizedBox(
+                            height: StopwatchConst.kButtonSize,
+                            width: StopwatchConst.kButtonSize,
+                            child: TimerButton(
+                              label: 'Lap',
+                              labelColor: Palette.kGreyText,
+                              color: Palette.kButtonDisabled,
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: SizedBox(
+                            height: StopwatchConst.kButtonSize,
+                            width: StopwatchConst.kButtonSize,
+                            child: TimerButton(
+                              label: 'Start',
+                              labelColor: Palette.kGreenText,
+                              color: Palette.kButtonGreen,
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
                       ],
                     );
                   }),

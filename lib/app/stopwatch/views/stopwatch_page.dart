c5f7d9f72c7bcbce2d15abc1dcba9ade396fc55f
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-12-09 11:14:37 +0800

    intialises provider on stopwatch page

--- lib/app/stopwatch/views/stopwatch_page.dart
+++ lib/app/stopwatch/views/stopwatch_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../view_model/stopwatch_viewmodel.dart';
 import 'views.dart';
 
 class StopwatchPage extends StatelessWidget {
@@ -7,26 +9,29 @@ class StopwatchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: AspectRatio(
-                aspectRatio: 1 / 1,
-                child: LayoutBuilder(builder: (context, constraints) {
-                  final radius = constraints.maxWidth / 2;
-                  return Stack(
-                    children: [
-                      StopwatchRender(radius: radius),
-                      StopwatchTickerUI(radius: radius),
-                    ],
-                  );
-                }),
+    return ChangeNotifierProvider(
+      create: (_) => StopwatchViewModel(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    final radius = constraints.maxWidth / 2;
+                    return Stack(
+                      children: [
+                        StopwatchRender(radius: radius),
+                        StopwatchTickerUI(radius: radius),
+                      ],
+                    );
+                  }),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

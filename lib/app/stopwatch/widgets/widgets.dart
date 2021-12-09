e953b66d1928d5c3eea8ae4c8f6f158044541bb7
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-12-09 09:29:03 +0800

    cleans up barrel file exports

--- lib/app/stopwatch/widgets/widgets.dart
+++ lib/app/stopwatch/widgets/widgets.dart
@@ -1,5 +1,5 @@
 export 'elapsed_time_text.dart';
+export 'stopwatch_hand.dart';
 export 'stopwatch_mins_tick_marker.dart';
-export 'stopwatch_secs_hand.dart';
 export 'stopwatch_secs_tick_marker.dart';
 export 'stopwatch_text_marker.dart';

e6d39dbbe53b82536ecf9e0137daf66237120c6e
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-12-10 15:53:45 +0800

    adds beamer locations and keys

--- lib/core/router/beam_locations.dart
+++ lib/core/router/beam_locations.dart
@@ -1,7 +1,31 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/widgets.dart';
 
+import '../../app/home/home.dart';
 import '../../app/stopwatch/views/stopwatch_page.dart';
+import '../../app/world_clock/world_clock_page.dart';
+
+class HomeLocation extends BeamLocation {
+  @override
+  List<BeamPage> buildPages(
+      BuildContext context, RouteInformationSerializable state) {
+    return [HomeWrapper.beamLocation];
+  }
+
+  @override
+  List<Pattern> get pathPatterns => [HomeWrapper.path];
+}
+
+class WorldClockLocation extends BeamLocation {
+  @override
+  List<BeamPage> buildPages(
+      BuildContext context, RouteInformationSerializable state) {
+    return [WorldClockPage.beamLocation];
+  }
+
+  @override
+  List<Pattern> get pathPatterns => [WorldClockPage.path];
+}
 
 class StopwatchLocation extends BeamLocation {
   @override
